4d78762ec74006a0ee991ee7fbd2fb704f431173
jonathantang <jonathantang0126@gmail.com>
2019-10-23 21:36:39 +0800

    connect url database

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ const port = 3000
 const Url = require('./models/url')
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/shortenerURL')
+mongoose.connect('mongodb://localhost/shortenerURL', { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-
 
 app.get('/', (req, res) => {
   res.render('index')
