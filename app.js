1cd2ce1dab690cadd88b774c910cb6a03e222d57
jonathantang <jonathantang0126@gmail.com>
2019-10-24 01:18:06 +0800

    add home routers

--- app.js
+++ app.js
@@ -28,59 +28,7 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-//路由設定
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-//轉址路由
-app.get('/:shortenUrl', (req, res) => {
-  const shortenUrl = req.params.shortenUrl
-  Url.findOne({ shortenUrl: shortenUrl })
-    .then(url => {
-      if (!url) {
-        return res.send('not find')
-      } else {
-        res.redirect(url.originalUrl)
-      }
-    })
-    .catch((err) => {
-      console.error(err)
-    })
-
-})
-
-//縮網址路由
-app.post('/', (req, res) => {
-  const hostName = req.headers.origin
-  const originalUrl = req.body.inputURL
-
-  if (!originalUrl) {
-    const errorMessage = "你沒填寫要縮的網址喔！"
-    return res.render('index', { errorMessage })
-  } else {
-    Url.findOne({ originalUrl: originalUrl })
-      //查詢原始網址 若已存在直接輸出縮網址
-      .then(url => {
-        if (url) {
-          return res.render('index', { url: url, hostName })
-        } else {
-          //不存在資料庫 轉成縮網址後輸出
-          const shortenUrl = randomstring.generate(5)
-          const url = new Url({
-            originalUrl: originalUrl,
-            shortenUrl: shortenUrl
-          })
-
-          url.save(err => {
-            if (err) return console.error(err)
-            return res.render('index', { url: url, hostName })
-          })
-        }
-      })
-  }
-})
-
+app.use('/', require('./routes/home'))
 
 
 app.listen(port, () => {
---
+++ routes/home.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const router = express.Router()
+const Url = require('../models/url')
+
+//路由設定
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+//轉址路由
+router.get('/:shortenUrl', (req, res) => {
+  const shortenUrl = req.params.shortenUrl
+  Url.findOne({ shortenUrl: shortenUrl })
+    .then(url => {
+      if (!url) {
+        return res.send('not find')
+      } else {
+        res.redirect(url.originalUrl)
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+
+})
+
+//縮網址路由
+router.post('/', (req, res) => {
+  const hostName = req.headers.origin
+  const originalUrl = req.body.inputURL
+
+  if (!originalUrl) {
+    const errorMessage = "你沒填寫要縮的網址喔！"
+    return res.render('index', { errorMessage })
+  } else {
+    Url.findOne({ originalUrl: originalUrl })
+      //查詢原始網址 若已存在直接輸出縮網址
+      .then(url => {
+        if (url) {
+          return res.render('index', { url: url, hostName })
+        } else {
+          //不存在資料庫 轉成縮網址後輸出
+          const shortenUrl = randomstring.generate(5)
+          const url = new Url({
+            originalUrl: originalUrl,
+            shortenUrl: shortenUrl
+          })
+
+          url.save(err => {
+            if (err) return console.error(err)
+            return res.render('index', { url: url, hostName })
+          })
+        }
+      })
+  }
+})
+
+
+
+module.exports = router
